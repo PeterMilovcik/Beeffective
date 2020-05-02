d771fa9915dc42e14c6b2af7fd55b9bce21adc69
Peter Milovcik <peter.milovcik@gmail.com>
2020-05-02 14:37:52 +0200

    TimeTracker

---
+++ Beeffective.Core/Time/TimeTracker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Timers;
+
+namespace Beeffective.Core.Time
+{
+    [Export]
+    public class TimeTracker
+    {
+        private readonly Timer timer;
+        
+        public TimeTracker()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimerElapsed;
+        }
+
+        public DateTime StartTime { get; private set; }
+
+        public DateTime StopTime { get; private set; }
+
+        public void StartTimer()
+        {
+            StartTime = DateTime.Now;
+            timer.Start();
+            OnStarted();
+        }
+
+        protected virtual void OnStarted() => 
+            Started?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler Started;
+
+        public void StopTimer()
+        {
+            timer.Stop();
+            StopTime = DateTime.Now;
+            OnStopped();
+        }
+
+        public event EventHandler Stopped;
+
+        protected virtual void OnStopped() => 
+            Stopped?.Invoke(this, EventArgs.Empty);
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e) => 
+            OnTicked();
+
+        protected virtual void OnTicked() => 
+            Ticked?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler Ticked;
+    }
+}
