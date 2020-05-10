8490e88f97533689736ef1a53461daf6e6d9c020
Peter Milovcik <peter.milovcik@gmail.com>
2020-05-10 14:44:39 +0200

    Changed position of always on top window

--- Beeffective.Presentation/AlwaysOnTop/AlwaysOnTopWindow.xaml.cs
+++ Beeffective.Presentation/AlwaysOnTop/AlwaysOnTopWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.Composition;
-using System.Windows;
+﻿using System.ComponentModel.Composition;
 using System.Windows.Input;
 
 namespace Beeffective.Presentation.AlwaysOnTop
@@ -8,34 +6,9 @@ namespace Beeffective.Presentation.AlwaysOnTop
     [Export(typeof(IAlwaysOnTopWindow))]
     public partial class AlwaysOnTopWindow : IAlwaysOnTopWindow
     {
-        private const int LeftOffset = -200;
-
         public AlwaysOnTopWindow()
         {
             InitializeComponent();
-        }
-
-        protected override void OnInitialized(EventArgs e)
-        {
-            base.OnInitialized(e);
-            UpdatePosition();
-        }
-
-        protected override void OnActivated(EventArgs e)
-        {
-            base.OnActivated(e);
-            UpdatePosition();
-        }
-
-        private void AlwaysOnTopWindow_OnSizeChanged(object sender, SizeChangedEventArgs e) => 
-            UpdatePosition();
-
-        private void UpdatePosition()
-        {
-            Left = double.IsNaN(Width)
-                ? SystemParameters.WorkArea.Width + LeftOffset
-                : SystemParameters.WorkArea.Width + LeftOffset - Width;
-            Top = 0;
         }
 
         private void AlwaysOnTopWindow_OnMouseMove(object sender, MouseEventArgs e)
