9d95e773207f81df40cdfd09e2f78c260e03483a
Peter Milovcik <peter.milovcik@gmail.com>
2020-05-13 11:31:44 +0200

    Dashboard with priorities

--- Beeffective.Presentation/Main/Dashboard/DashboardViewModel.cs
+++ Beeffective.Presentation/Main/Dashboard/DashboardViewModel.cs
@@ -1,15 +1,41 @@
-﻿using System.ComponentModel.Composition;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
 using Beeffective.Presentation.Common;
 using Beeffective.Presentation.Main.Priority;
+using Beeffective.Presentation.Main.Tasks;
 
 namespace Beeffective.Presentation.Main.Dashboard
 {
     [Export]
     public class DashboardViewModel : ContentViewModel
     {
+        private ObservableCollection<TaskViewModel> priorityCollection;
+
         [ImportingConstructor]
         public DashboardViewModel(PriorityObservableCollection tasks) : base(tasks)
         {
+            Tasks.CollectionChanged += (sender, args) => Update();
         }
+
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+            Update();
+        }
+
+        private void Update()
+        {
+            PriorityCollection = new ObservableCollection<TaskViewModel>(
+                Tasks.OrderBy(t => t.Model.Priority));
+        }
+
+        public ObservableCollection<TaskViewModel> PriorityCollection
+        {
+            get => priorityCollection;
+            set => SetProperty(ref priorityCollection, value);
+        }
+
     }
 }
