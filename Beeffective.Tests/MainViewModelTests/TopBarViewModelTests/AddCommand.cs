30699af6475377e56c76f92d4e7aaf2bb5a31c5c
Peter Milovcik <peter.milovcik@gmail.com>
2020-05-24 14:06:41 +0200

    Beeffective.Tests.MainViewModelTests.TopBarViewModelTests.AddCommand

---
+++ Beeffective.Tests/MainViewModelTests/TopBarViewModelTests/AddCommand.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Beeffective.Tests.MainViewModelTests.TopBarViewModelTests
+{
+    public class AddCommand : TestFixture
+    {
+        public override void OneTimeSetUp()
+        {
+            base.OneTimeSetUp();
+            MainViewModel.ShowAsync().GetAwaiter().GetResult();
+            SUT.AddCommand.Execute(null);
+        }
+
+        [Test]
+        public void IsAddMenuOpen_True() => SUT.IsAddMenuOpen.Should().BeTrue();
+    }
+}
