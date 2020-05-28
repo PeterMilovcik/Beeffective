8042d88e0810c4b88196e03b7a5692bba44aa00a
Peter Milovcik <peter.milovcik@gmail.com>
2020-05-28 13:56:11 +0200

    New Double for NewLabelView

---
+++ Beeffective.Tests/Doubles/NewLabelView.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.Composition;
+using Beeffective.Presentation.Main.Labels;
+
+namespace Beeffective.Tests.Doubles
+{
+    [Export(typeof(INewLabelView))]
+    public class NewLabelView : View, INewLabelView
+    {
+    }
+}
